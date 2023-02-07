7f6e98facc6eb3077279d696fbde7b869dee963c
Tata <lentstata@gmail.com>
2023-02-08 00:54:53 +0500

    второй коммит с рабочим кодом для проверки

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] allSales = {1, 3, 5, 7, 9, 11, 20};
+        SalesManager abracaadabra = new SalesManager(allSales);
+
+        System.out.println("Самая большая продажа: " + abracaadabra.max());
     }
 }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -15,4 +15,3 @@ public class SalesManager {
         return max;
     }
 }
-
