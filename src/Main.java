3c97559b7de3822531900d4cdf794146824e8b9c
Tata <lentstata@gmail.com>
2023-02-11 14:01:19 +0500

    расчёт обрезанного среднего

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int[] allSales = {1, 3, 5, 7, 9, 11, 20};
+        int[] allSales = {3, 3, 5, 7, 9, 19, 20};
         SalesManager abracaadabra = new SalesManager(allSales);
 
         System.out.println("Самая большая продажа: " + abracaadabra.max());
+        System.out.println("Самая маленькая продажа: " + abracaadabra.min());
+        System.out.println("Среднее количество продаж: " + abracaadabra.stat());
     }
 }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -14,4 +14,23 @@ public class SalesManager {
         }
         return max;
     }
+    public int min() {
+        int min = sales [0];
+        for (int sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+
+    public int stat () {
+        int mean = 0;
+        for (int i = 0; i < sales.length; i++) {
+        mean += sales[i];
+    }
+    mean = (mean - max() - min()) / (sales.length - 2);
+        return mean;
+}
+
 }
