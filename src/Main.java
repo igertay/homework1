d759096c235ad704b26ac0b76e24f34e4e8b15b8
Tata <lentstata@gmail.com>
2023-02-11 13:00:39 +0500

    ÐИзменениÑе типа int на long

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] allSales = {1, 3, 5, 7, 9, 11, 20};
+        long[] allSales = {1, 3, 5, 7, 9, 11, 20};
         SalesManager abracaadabra = new SalesManager(allSales);
 
         System.out.println("Самая большая продажа: " + abracaadabra.max());
